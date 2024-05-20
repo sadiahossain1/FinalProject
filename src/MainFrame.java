b1be3670bf0648dbabc22645b79e7f18865121a8
sadiahossain1 <sadiah16@nycstudents.net>
2024-05-20 12:31:18 -0400

    working

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MainFrame {
+import javax.swing.*;
+public class MainFrame  {
+    public MainFrame() {
+        JFrame frame = new JFrame("Intro to Animation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(768, 512);
+        frame.setLocation(500, 200);
+
+        // create and add panel
+        GraphicsPanel panel = new GraphicsPanel();
+        frame.add(panel);
+
+        // display the frame
+        frame.setVisible(true);
+    }
 }
