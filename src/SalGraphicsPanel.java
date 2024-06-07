1a131986aec6b5bd9f41b56ad829ade95d6f1ccf
sadiahossain1 <sadiah16@nycstudents.net>
2024-06-07 12:26:30 -0400

    working on snakes and ladders

--- src/SalGraphicsPanel.java
+++ src/SalGraphicsPanel.java
@@ -16,32 +16,33 @@ public class  SalGraphicsPanel extends JPanel {
     };
     private int width = 65;
     private int height = 70;
-    private int count = 100;
+    private int count = 1;
 
     public SalGraphicsPanel() {
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for (int r = 0; r < grid.length; r++) {
+        for (int r = grid.length - 1; r >= 0; r--) {
             for (int c = 0; c < grid[0].length; c++) {
                 if (grid[r][c] == 0) {
-                    g.setColor(Color.BLUE);
+                    g.setColor(Color.YELLOW);
                     g.fillRect(c * width, r * height, width, height);
-                    g.setColor(Color.WHITE);
-                    g.setFont(new Font("Courier New", Font.BOLD, 18));
-                    g.drawString(count + "", c * width, r * height);
-                    count--;
                 }
                 if (grid[r][c] == 1) {
-                    g.setColor(Color.black);
-                    g.fillRect(c * width, r * height, width, height);
                     g.setColor(Color.WHITE);
-                    g.setFont(new Font("Courier New", Font.BOLD, 18));
-                    g.drawString(count + "", c*width, r*height);
-                    count--;
+                    g.fillRect(c * width, r * height, width, height);
                 }
+                g.setColor(Color.BLACK);
+                g.setFont(new Font("Courier New", Font.BOLD, 18));
+                g.drawString(count - 100 + "", c*width+10, r*height+20);
+                count++;
             }
         }
+        g.setColor(Color.BLUE);
+        g.fillOval(10, 650, 45, 45);
+        // Draw player 2
+        g.setColor(Color.PINK);
+        g.fillOval(20, 630, 45, 45);
     }
 }
